97a882b36fa9572cfbd96090f9f570c1779a8757
Cody Johnson <codyj@protonmail.com>
2018-12-27 14:31:26 -0500

    Fix bug

--- client/src/components/Index/StandingsItemTable.vue
+++ client/src/components/Index/StandingsItemTable.vue
@@ -47,7 +47,7 @@ export default {
                 13: 'FLA', 14: 'TBL', 15: 'WSH', 16: 'CHI', 17: 'DET',
                 18: 'NSH', 19: 'STL', 20: 'CGY', 21: 'COL', 22: 'EDM',
                 23: 'VAN', 24: 'ANA', 25: 'DAL', 26: 'LAK', 28: 'SJS',
-                29: 'CBJ', 30: 'MIN', 52: 'WPG', 53: 'ARI', 54: 'VKG'
+                29: 'CBJ', 30: 'MIN', 52: 'WPG', 53: 'ARI', 54: 'VGK'
             },
             headers: ['Team', 'GP', 'W', 'L', 'OT', 'P', 'STK']
         }
